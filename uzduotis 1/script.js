9f8bfdb7419837751b175208773b2af1e7c7c33e
edgo1 <92423441+edgo1@users.noreply.github.com>
2022-06-15 12:47:23 +0300

    pataisytas pirmas uzdavinys

--- uzduotis 1/script.js
+++ uzduotis 1/script.js
@@ -1,14 +1,19 @@
 window.onload = function() {
     const pazymiai = [5,8,6,4,7,5,10,8,10,9]
-    console.log(pazymiuVidurkis(pazymiai))
-
+    let suskaiciuotas = masyvoSuma(pazymiai);
+    //console.log(suma(pazymiai));
+    console.log(dalyba(pazymiai))
+    
 }
 
-function pazymiuVidurkis(masyvas) {
+function masyvoSuma(masyvas) {
     let suma = 0;
     for (let i = 0; i < masyvas.length; i++) {
         suma += masyvas[i];
 }
-return suma /= masyvas.length
+return suma;
 
+}
+function dalyba (skaicius){
+    return masyvoSuma(skaicius) / skaicius.length
 }
