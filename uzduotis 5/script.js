d64160b33139249ed730146210a47df4e2ea4baa
edgo1 <92423441+edgo1@users.noreply.github.com>
2022-06-15 14:57:37 +0300

    --

--- uzduotis 5/script.js
+++ uzduotis 5/script.js
@@ -1,11 +1,10 @@
 window.onload = function() {
     const skarry = [5, 12, 3, 6, 8, 14, 15, 5, 6, 10];
-    let isRingtiSkaiciai = didesniSkaiciaiUz10(skarry,5);
+    let isRingtiSkaiciai = didesniSkaiciaiUz10(skarry,8);
     console.log(isRingtiSkaiciai);
 }
 
 function didesniSkaiciaiUz10(skaicius,y){
-    const masyvas = [];
     let skaiciuSuma = 0;
     for (let i = 0; i < skaicius.length; i++) {
         if(arSkaiciusDaugiauUz10(skaicius[i], y)){
