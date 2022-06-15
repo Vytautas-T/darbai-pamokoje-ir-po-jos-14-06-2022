f221cd33d2dc3139a55c4f2b4b223701153c7aa6
edgo1 <92423441+edgo1@users.noreply.github.com>
2022-06-15 14:51:56 +0300

    prideta paskutine funcija

--- uzduotis 5/script.js
+++ uzduotis 5/script.js
@@ -1,20 +1,20 @@
 window.onload = function() {
     const skarry = [5, 12, 3, 6, 8, 14, 15, 5, 6, 10];
-    const isRingtiSkaiciai = didesniSkaiciaiUz10(skarry);
+    let isRingtiSkaiciai = didesniSkaiciaiUz10(skarry,5);
     console.log(isRingtiSkaiciai);
 }
 
-function didesniSkaiciaiUz10(skaicius){
+function didesniSkaiciaiUz10(skaicius,y){
     const masyvas = [];
     let skaiciuSuma = 0;
     for (let i = 0; i < skaicius.length; i++) {
-        if(arSkaiciusDaugiauUz10(skaicius[i])){
+        if(arSkaiciusDaugiauUz10(skaicius[i], y)){
             skaiciuSuma++;
         }
     }
     return skaiciuSuma;
 }
 
-function arSkaiciusDaugiauUz10(x) {
-    return x > 10;
+function arSkaiciusDaugiauUz10(x,y) {
+    return x > y;
 }
